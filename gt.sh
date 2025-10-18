f5e53cc2e0c85b693d211a0bb7ce995b1f3afdae
raj.rachakonda <raj.rachakonda@resolvetech.com>
2025-10-18 00:46:56 +0000

    refactoring

--- gt.sh
+++ gt.sh
@@ -1,1 +1,1 @@
-git checkout main && git add . && git commit -m "refactoring" && git pull --rebase origin main && git push origin main
+git checkout main && git add . && git commit -m "refactoring" &&  git push origin main
